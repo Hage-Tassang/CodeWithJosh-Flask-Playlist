ac32f8b0cc9fd8dbc238fc0c81dc83440ecef4ff
Hage Tassang <hage51552@gmail.com>
2025-09-26 09:19:17 -0800

    fixed and updated , added more features
    features:drop down menu,navbar,seach bar and included lot more regions
    fixed my queries and added some more queries

--- flask-bootstrap-api/app.py
+++ flask-bootstrap-api/app.py
@@ -13,13 +13,18 @@ app =  Flask(__name__)
 @app.route('/')
 def index():
     query = request.args.get("query","latest")
+    sort_by = request.args.get('sortBy', 'popularity')
+    trending = f"https://newsapi.org/v2/everything?q={sort_by}&apiKey={NEWS_API_KEY}"
     url = f"https://newsapi.org/v2/everything?q={query}&apiKey={NEWS_API_KEY}"
     response = requests.get(url)
+    response2 = requests.get(trending)
+    trends = response2.json()
     news_data = response.json()
     #print(news_data)
+    
     articles = news_data.get('articles',[])
     filtered_articles =  [article for article in articles if "Yahoo" not in article['source']['name'] and "BBC" not in article['source']['name'] and "removed" not in article['source']['name'].lower()]
-    return render_template("index.html",articles=filtered_articles, query = query) 
+    return render_template("index.html",articles=filtered_articles, query = query,trends = trends) 
 
 if __name__ == "__main__":
     app.run(debug=True)
